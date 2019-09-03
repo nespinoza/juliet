4abf3c499466dc358736f549afabb40a20f98a8d
Nestor Espinoza <nsespino@uc.cl>
2019-09-03 12:54:56 +0200

    setup bump due to python2-3 upgrade.

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 from setuptools import setup
 
 setup(name='juliet',
-      version='2.0',
-      description='juliet: a versatile modelling tool for transiting exoplanets, RVs or both',
+      version='2.0.1',
+      description='juliet: a versatile modelling tool for transiting exoplanets, radial-velocity systems or both',
       url='http://github.com/nespinoza/juliet',
       author='Nestor Espinoza',
       author_email='espinoza@mpia.de',
       license='MIT',
       packages=['juliet'],
-      install_requires=['batman-package','radvel','dynesty','george','celerite','astropy'],
+      install_requires=['batman-package','radvel','dynesty','george','celerite','astropy','numpy','scipy'],
+      python_requires='>=2.7',
       extras_requires={
             'seaborn':['seaborn'],
             'pymultinest':['pymultinest'],
